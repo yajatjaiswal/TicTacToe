798d16cca93b7d2a509ff6c573cea0b981256183
yajatjaiswal <100556060+yajatjaiswal@users.noreply.github.com>
2022-06-10 12:14:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+var count = 1;
+
+function tap(control) {
+  if (count <= 9) {
+    if(document.getElementById(control.id).innerHTML == ""){
+
+      if (count % 2 != 0) {
+        document.getElementById(control.id).innerHTML = "X";
+      } else {
+        document.getElementById(control.id).innerHTML = "O";
+      }
+      count++;
+      if (checkWin()) {
+        alert("Winner");
+        reset();
+      }
+    }
+    } else {
+      alert("Match Draw");
+      reset();
+  }
+}
+function reset() {
+  for (var i = 1; i <= 9; i++) {
+    document.getElementById('div' + i).innerHTML = "";
+  }
+  count = 1;
+}
+
+function checkWin() {
+  if(checkCondition('div1','div2','div3')||checkCondition('div4','div5','div6')||checkCondition('div7','div8','div9')||checkCondition('div1','div4','div7')||checkCondition('div2','div5','div8')||checkCondition('div3','div6','div9')||checkCondition('div1','div5','div9')||checkCondition('div3','div5','div7'))
+  {
+      return true;
+  }
+  else{
+    if(count==10){
+      alert("Match Draw");
+      reset();
+    }
+  }
+}
+
+function checkCondition (div1,div2,div3)
+{
+  if (
+    getData(div1) != "" &&
+    getData(div2)!= "" && getData(div3)!= "" &&
+    getData(div1) ==
+      getData(div2)&&
+    getData(div2)==
+      getData(div3)
+  ) {
+    return true;
+  }
+}
+
+function getData(div)
+{
+  return document.getElementById(div).innerHTML;
+}
